600e98eb7c11fd6622f2b693e09ec4d237a8f0e8
Frankuku <franconicolasgerez1998@gmail.com>
2025-07-06 23:54:20 -0300

    Update orderRouter.js

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -7,22 +7,23 @@ import {
     cancelOrder
 } from '../controllers/orderController.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
+import {verifyAdminRole} from '../middlewares/verifyAdminRole.js';
 
 const router = Router();
 
-router.post('/', verifyToken, 
+router.post('/', verifyToken, verifyAdminRole,
     createOrder);
 
-router.get('/', verifyToken,
+router.get('/', verifyToken,verifyAdminRole,
     getOrders);
 
-router.get('/:id', verifyToken,
+router.get('/:id', verifyToken, verifyAdminRole,
     getOrderById);
 
-router.patch('/:id/status', verifyToken, 
+router.patch('/:id/status', verifyToken, verifyAdminRole,
     updateOrderStatus);
 
-router.post('/:id/cancel', verifyToken, 
+router.post('/:id/cancel', verifyToken, verifyAdminRole,
     cancelOrder);
 
 export default router;
